8ddac75c6b07a5cb05fa37b7704c24d8cf6bca11
JaiRaga <raga.j.santhosh@gmail.com>
2022-04-23 17:38:18 +0530

    :zap: finished login page

--- screens/Login.js
+++ screens/Login.js
@@ -1,8 +1,8 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { Formik } from 'formik';
-import { Octicons, Ionicons } from '@expo/vector-icons';
+import { Octicons, Ionicons, Fontisto } from '@expo/vector-icons';
 import {
   StyledContainer,
   InnerContainer,
@@ -15,58 +15,91 @@ import {
   StyledInputLabel,
   StyledTextInput,
   RighttIcon,
+  StyledButton,
+  ButtonText,
+  MsgBox,
+  Line,
+  ExtraView,
+  ExtraText,
+  TextLink,
+  TextLinkContent,
 } from './../components/styles';
 import foodLogo from './../assets/img/food2.jpg';
+import KeyboardAvoidingWrapper from '../components/KeyboardAvoidingWrapper';
 
-const { brand, darkLight } = Colors;
+const { brand, darkLight, primary } = Colors;
 
 const Login = () => {
-  const [hidePassword, setHidePassword] = useState(true)
+  const [hidePassword, setHidePassword] = useState(true);
   return (
-    <StyledContainer>
-      <StatusBar style="dark" />
-      <InnerContainer>
-        <PageLogo resizeMode="cover" source={foodLogo} />
-        <PageTitle>Uber Eats</PageTitle>
-        <SubTitle>Account Login</SubTitle>
-        <Formik
-          initialValues={{ email: '', password: '' }}
-          onSubmit={(values) => console.log(values)}
-        >
-          {({ handleChange, handleBlur, handleSubmit, values }) => (
-            <StyledFormArea>
-              <MyTextInput
-                label="Email Address"
-                icon="mail"
-                placeholder="Krishna@Pappa.com"
-                placeholderTextColor={darkLight}
-                onChangeText={handleChange('email')}
-                onBlur={handleBlur('email')}
-                value={values.email}
-                keyboardType="email-address"
-              />
-              <MyTextInput
-                label="Password"
-                icon="lock"
-                placeholder="* * * * * * * * * *"
-                placeholderTextColor={darkLight}
-                onChangeText={handleChange('password')}
-                onBlur={handleBlur('password')}
-                value={values.password}
-                secureTextEntry={hidePassword}
-                isPassword={true}
-                hidePassword={hidePassword}
-                setHidePassword={setHidePassword}
-              />
-            </StyledFormArea>
-          )}
-        </Formik>
-      </InnerContainer>
-    </StyledContainer>
+    <KeyboardAvoidingWrapper>
+      <StyledContainer>
+        <StatusBar style="dark" />
+        <InnerContainer>
+          <PageLogo resizeMode="cover" source={foodLogo} />
+          <PageTitle>Uber Eats</PageTitle>
+          <SubTitle>Account Login</SubTitle>
+          <Formik
+            initialValues={{ email: '', password: '' }}
+            onSubmit={(values) => console.log(values)}
+          >
+            {({ handleChange, handleBlur, handleSubmit, values }) => (
+              <StyledFormArea>
+                <MyTextInput
+                  label="Email Address"
+                  icon="mail"
+                  placeholder="Krishna@test.com"
+                  placeholderTextColor={darkLight}
+                  onChangeText={handleChange('email')}
+                  onBlur={handleBlur('email')}
+                  value={values.email}
+                  keyboardType="email-address"
+                />
+                <MyTextInput
+                  label="Password"
+                  icon="lock"
+                  placeholder="* * * * * * * * * *"
+                  placeholderTextColor={darkLight}
+                  onChangeText={handleChange('password')}
+                  onBlur={handleBlur('password')}
+                  value={values.password}
+                  secureTextEntry={hidePassword}
+                  isPassword={true}
+                  hidePassword={hidePassword}
+                  setHidePassword={setHidePassword}
+                />
+                <MsgBox>...</MsgBox>
+                <StyledButton onPress={handleSubmit}>
+                  <ButtonText>Login</ButtonText>
+                </StyledButton>
+                <Line />
+                <StyledButton google={true} onPress={handleSubmit}>
+                  <Fontisto name="google" size={25} color={primary} />
+                  <ButtonText google={true}>Sign in with Google</ButtonText>
+                </StyledButton>
+                <ExtraView>
+                  <ExtraText>Don't have an account already? </ExtraText>
+                  <TextLink>
+                    <TextLinkContent>Signup</TextLinkContent>
+                  </TextLink>
+                </ExtraView>
+              </StyledFormArea>
+            )}
+          </Formik>
+        </InnerContainer>
+      </StyledContainer>
+    </KeyboardAvoidingWrapper>
   );
 };
 
-const MyTextInput = ({ label, icon, isPassword, hidePassword, setHidePassword, ...props }) => {
+const MyTextInput = ({
+  label,
+  icon,
+  isPassword,
+  hidePassword,
+  setHidePassword,
+  ...props
+}) => {
   return (
     <View>
       <LeftIcon>
@@ -76,7 +109,11 @@ const MyTextInput = ({ label, icon, isPassword, hidePassword, setHidePassword, .
       <StyledTextInput {...props} />
       {isPassword && (
         <RighttIcon onPress={() => setHidePassword(!hidePassword)}>
-            <Ionicons name={hidePassword ? 'md-eye-off' : "md-eye"} size={30} color={darkLight} />
+          <Ionicons
+            name={hidePassword ? 'md-eye-off' : 'md-eye'}
+            size={30}
+            color={darkLight}
+          />
         </RighttIcon>
       )}
     </View>
