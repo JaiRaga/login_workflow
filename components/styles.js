25f325e15491e0c4044da976544ff7537e4ead40
JaiRaga <raga.j.santhosh@gmail.com>
2022-04-23 17:36:19 +0530

    :sparkles: added styles for signup and welcome page

--- components/styles.js
+++ components/styles.js
@@ -29,6 +29,28 @@ export const InnerContainer = styled.View`
   align-items: center;
 `;
 
+export const WelcomeContainer = styled(InnerContainer)`
+  padding: 25px;
+  padding-top: 10px;
+  justify-content: center;
+`;
+
+export const Avatar = styled.Image`
+  width: 100px;
+  height: 100px;
+  margin: auto;
+  border-radius: 50px;
+  border-width: 2px;
+  border-color: ${secondary};
+  margin-bottom: 10px;
+  margin-top: 10px;
+`;
+
+export const WelcomeImage = styled.Image`
+  height: 50%;
+  min-width: 100%;
+`;
+
 export const PageLogo = styled.Image`
   width: 300px;
   height: 200px;
@@ -40,6 +62,12 @@ export const PageTitle = styled.Text`
   font-weight: bold;
   color: ${brand};
   padding: 10px;
+
+  ${(props) =>
+    props.welcome &&
+    `
+    font-size: 35px;
+  `}
 `;
 
 export const SubTitle = styled.Text`
@@ -48,6 +76,13 @@ export const SubTitle = styled.Text`
   letter-spacing: 1px;
   font-weight: bold;
   color: ${tertiary};
+
+  ${(props) =>
+    props.welcome &&
+    `
+    margin-bottom: 5px;
+    font-weight: normal;
+  `}
 `;
 
 export const StyledFormArea = styled.View`
@@ -70,32 +105,82 @@ export const StyledInputLabel = styled.Text`
   color: ${tertiary};
   font-size: 13px;
   text-align: left;
-`
+`;
 
 export const LeftIcon = styled.View`
   position: absolute;
   left: 15px;
   top: 38px;
-  z-index: 1
-`
+  z-index: 1;
+`;
 
 export const RighttIcon = styled.TouchableOpacity`
   position: absolute;
   right: 15px;
   top: 38px;
-  z-index: 1
-`
+  z-index: 1;
+`;
 
 export const StyledButton = styled.TouchableOpacity`
   padding: 15px;
   background-color: ${brand};
   justify-content: center;
+  align-items: center;
   border-radius: 5px;
   margin-vertical: 5px;
   height: 60px;
-`
+
+  ${(props) =>
+    props.google === true &&
+    `
+    background-color: ${green};
+    flex-direction: row;
+    justify-content: center;
+  `}
+`;
 
 export const ButtonText = styled.Text`
   color: ${primary};
   font-size: 16px;
-`
+
+  ${(props) =>
+    props.google === true &&
+    `
+    padding: 5px;
+  `}
+`;
+export const MsgBox = styled.Text`
+  text-align: center;
+  font-size: 13px;
+`;
+
+export const Line = styled.View`
+  height: 1px;
+  width: 100%;
+  background-color: ${darkLight};
+  margin-vertical: 10px;
+`;
+
+export const ExtraView = styled.View`
+  justify-content: center;
+  flex-direction: row;
+  align-items: center;
+  padding: 10px;
+`;
+
+export const ExtraText = styled.Text`
+  justify-content: center;
+  align-items: center;
+  color: ${tertiary};
+  font-size: 15px;
+`;
+
+export const TextLink = styled.TouchableOpacity`
+  justify-content: center;
+  align-items: center;
+`;
+
+export const TextLinkContent = styled.Text`
+  color: ${brand};
+  font-size: 15px;
+`;
